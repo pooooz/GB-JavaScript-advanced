04ab050ec5a6858a067cd9e97407f0b34b8d208f
POZ <antoivans@gmail.com>
2022-03-05 14:21:03 +0300

    Added conditions for the third task

---
+++ task-3/js/main.js
@@ -0,0 +1,50 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+class ProductsList {
+  constructor(container = '.products'){
+    this.container = container;
+    this.goods = []; //массив товаров из JSON документа
+    this._getProducts()
+      .then(data => { //data - объект js
+        this.goods = data;
+        this.render();
+      });
+  }
+
+  _getProducts(){
+    return fetch(`${API}/catalogData.json`)
+      .then(result => result.json())
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render(){
+    const block = document.querySelector(this.container);
+    for (let product of this.goods){
+      const productObj = new ProductItem(product);
+      block.insertAdjacentHTML('beforeend', productObj.render());
+    }
+  }
+}
+
+class ProductItem {
+  constructor(product, img = 'https://via.placeholder.com/200x150'){
+    this.title = product.product_name;
+    this.price = product.price;
+    this.id = product.id_product;
+    this.img = img;
+  }
+  render(){
+    return `<div class="product-item" data-id="${this.id}">
+            <img src="${this.img}" alt="Some img">
+            <div class="desc">
+                <h3>${this.title}</h3>
+                <p>${this.price} $</p>
+                <button class="buy-btn">Купить</button>
+            </div>
+        </div>`
+  }
+}
+
+let list = new ProductsList();
