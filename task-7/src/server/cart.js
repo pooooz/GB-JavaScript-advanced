82234b500c7a7ab9467cbd3fa294baef45331042
POZ <antoivans@gmail.com>
2022-03-30 23:08:41 +0300

    Refactoring of cart.js in the seventh task

--- task-7/src/server/cart.js
+++ task-7/src/server/cart.js
@@ -17,11 +17,7 @@ let remove = (cart, req) => {
   let find = cart.contents.find(el => el.id_product === +req.params.id);
   cart.countGoods -= req.body.quantity;
   cart.amount -= find.price;
-  if (find.quantity > 1) {
-    find.quantity -= req.body.quantity;
-  } else {
-    cart.contents.splice(cart.contents.indexOf(find), 1);
-  }
+  cart.contents.splice(cart.contents.indexOf(find), 1);
   return JSON.stringify(cart, null, 2);
 }
 
